27ed17da28a47e15fa7449f741fa950e14d340ee
SnehAl2o7 <snehalsingh353@gmail.com>
2025-06-29 14:32:21 +0530

    Day-1 of learning Javascript

---
+++ WEbsite/script.js
@@ -0,0 +1,21 @@
+// console.log(`Hello`);
+// console.log(`I like to play`)
+
+// window.alert(`An alert`);
+// window.alert(`this si a page`);
+
+// comment
+
+/*  this is a multiple line comment*/
+
+document.getElementById("my").textContent = `Hello there`;
+document.getElementById("my0").textContent = `this is a paragraph`;
+ 
+
+
+
+
+
+
+
+
