3dfce7b09a67071750a738c93d7a04e0e3957896
SnehAl2o7 <snehalsingh353@gmail.com>
2025-07-13 16:14:11 +0530

    learning the date objects

---
+++ Day-9JS/DateObject.js
@@ -0,0 +1,7 @@
+// Date Objects = Objects that contain values that represent dates and times
+//                these date objects can be changed and formatted.
+
+const date = new Date(2024,0,20,14,3,24);
+const date1 = new Date("2024-01-20")
+console.log(date); 
+console.log(date1);
