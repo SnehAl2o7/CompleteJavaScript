7204ae397b7c04675c07e6834bf7fdb4c14bb738
SnehAl2o7 <snehalsingh353@gmail.com>
2025-07-14 14:21:42 +0530

    learning about the closure

--- Day-9JS/DateObject.js
+++ Day-9JS/DateObject.js
@@ -5,3 +5,39 @@ const date = new Date(2024,0,20,14,3,24);
 const date1 = new Date("2024-01-20")
 console.log(date); 
 console.log(date1);
+
+// learning the closures 
+
+/*
+closure = A function defined iside of another function,
+          the inner function has access to the variables
+          and scope of the outer function.
+          Allow for private variables and state maintenance
+          Used frequently in JS frameworks: React, Vue, Angluar
+*/
+
+function outer(){
+    let message = "Hello";
+
+    function inner(){
+        console.log(message);
+    }
+
+    inner();
+}
+
+outer();
+
+message = "Goodbye";   // no change as scope is differenet
+
+// closure will make the variables private 
+// implement the concept of abstraction & security
+
+function increment(){
+    let cnt = 0;
+    cnt++;
+
+    console.log(`Count increased to ${cnt}`);
+}
+
+increment();
