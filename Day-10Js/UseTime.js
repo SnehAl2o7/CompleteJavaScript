6b94d7821bcd3ae7fdb795187477b5e79832979d
SnehAl2o7 <snehalsingh353@gmail.com>
2025-07-14 14:52:31 +0530

    learning about time stamp

--- Day-10Js/UseTime.js
+++ Day-10Js/UseTime.js
@@ -18,4 +18,40 @@ const Time = setTimeout(function(){console.log("HI")}, 2000);
 
 setTimeout(() => console.log("yeps"), 1500);
 
-clearTimeout(Time)
+clearTimeout(Time)                              
+
+// learning the console.time()
+
+/* 
+console.time() = tool that allows us to measure the time it
+                 takes for a section of code or process to execute
+                 Great for identifying performance "bottlenecks"
+
+
+
+console.time("Label")
+cosole.timeEnd("label")
+*/
+
+console.time("test");
+
+for(let i = 0; i < 10000000000;i++){
+    // do some code
+}
+
+console.timeEnd("test");
+
+
+function loaddata(){
+    console.time("loacaldata");
+    for(let i = 0; i< 10000000000;i++){
+        //pretend to load some data
+
+    }
+
+    console.timeEnd("loacldata");
+}
+
+
+
+
